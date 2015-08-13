32dda42e0b5b640cc19603ae925900dcf86e6fb7
Tony L. Kerz <tony@WA716948A-HFD-OEM.local>
2015-08-12 22:00:00 -0400

    [dockerfile] gen docker file based on procfile

--- dcos-util.js
+++ dcos-util.js
@@ -1,7 +1,10 @@
+#!/usr/bin/env node
+
 var program = require('commander')
 
 program
 .version('0.0.1')
 .command('id [idString]', 'id from string')
 .command('marathon-json', 'generate marathon json')
+.command('dockerfile', 'generate dockerfile (requires Procfile)')
 .parse(process.argv)
